a1e75587ba53ee2ba2dc91c6206202a75e5b1116
Nick Lawler <lawler145@gmail.com>
2015-03-13 10:46:47 -0700

    stuff

--- symbol_table.h
+++ symbol_table.h
@@ -62,7 +62,7 @@ int symtab_entry_is_int(int index) {
 
 int symtab_put(char* name, int type, int istemp) {
     if (symtab.initialized) {
-        printf("inserting %s\n", name);
+        
         int index = symtab_get(name);
         int not_present = (index == -1);
 
@@ -72,6 +72,7 @@ int symtab_put(char* name, int type, int istemp) {
             symtab.st[symtab.length].type = type;
             symtab.st[symtab.length].istemp = istemp;
         } else {
+            printf("not inserting %s\n", name);
             return 1;
         }
 
